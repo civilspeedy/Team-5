71b87041394e65f29441535875364d5eeb392e60
Charlie <106312897+civilspeedy@users.noreply.github.com>
2022-11-22 11:35:59 +0000

    began encryption functions

--- Team 5/Back-End/main.js
+++ Team 5/Back-End/main.js
@@ -1,8 +1,14 @@
 const { callbackify } = require('util');
 
-function getLogin(){
-    var password = document.getElementById("password");
-    var username = document.getElementById("username");
+function getLogin(choice){
+    if (choice = "username"){
+        var username = document.getElementById("username");
+        return username
+    }
+    if (choice = "password"){
+        var password = document.getElementById("password");
+        return password
+    }
 }
 
 /**this function will take an input and store it within chosen json file */
@@ -17,4 +23,25 @@ function writeToJson(jsonFile, data){
         fs.writeFile(jsonFile, json, 'utf8', callback); 
     }});
 
+}
+
+//**Encrypts and returns data */
+function encryption(data){
+    var encryptedData = SubtleCrypto.encryption()
+}
+
+/**Generates encryption key.
+ * Reference:https://docs.w3cub.com/dom/subtlecrypto/generatekey
+*/
+function generateKey(){
+    let keyPair = SubtleCrypto.generateKey(
+        {
+            name: "RSA-OAEP",
+            modulusLengthm,
+            publicExponent: new Uint8Array([1, 0, 1]),
+            hash:"SHA-256"
+        },
+            true,
+            ["encrypt", "decrypt"]
+    );
 }
