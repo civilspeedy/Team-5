71894f647e7a70d14698d5b773776779e4d73946
Charlie <106312897+civilspeedy@users.noreply.github.com>
2022-11-29 11:21:51 +0000

    got the basic for loop for populating the page up. Still struggling with reading json, through it would be easier but guess I was wrong.

--- Team 5/Front-End/scripting.js
+++ Team 5/Front-End/scripting.js
@@ -1,19 +1,30 @@
+let products = [{
+    name: "Lamp",
+ price: 59.99,
+ quantity: 10,
+ image: "Images/lamp.jpg"},
+{name: "desk",
+price: 120.99,
+quantity: 5,
+image: "desk.png"
+}]; //can't get json reading working yet so this is test data for now
+
 /**This function will fill the page with products from datastore */
 function populate(){    
     let pageProducts = "";
-    for (product in products){
-        pageProducts += product;
+    var productDisplay = document.getElementById("productDisplay");
+    for (let product in products){
+        var productData = products[product];
+        pageProducts += "<div class='product'><img src=" + productData.image + "><br>\n<p class='productName'>" + productData.name + "</p><br>\n<p class='productPrice'>" + productData.price + "</p></div>";
     }
+    productDisplay.innerHTML = pageProducts;
 }
 
+/**This function is meant to fetch a json file */
 function getfile(filename){
     fetch("./Back-End/json/products.json")
         .then((response) => response.json())
-        .then((json) => console.log(json))
+        .then((json) => console.log(json));
 }
 
-function testFile(){
-    console.log(products);
-}
-
-getfile("../Back-End/json/products.json")
+populate();
